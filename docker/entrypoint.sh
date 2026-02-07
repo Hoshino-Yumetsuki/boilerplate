6b44cde3ff048d03628eea236630ca62cd62b01d
Hoshino-Yumetsuki <hoshino-yumetsuki@outlook.com>
2026-02-07 18:59:20 +0800

    fix: 修复安装脚本

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,6 +6,7 @@ chown -R root:root /koishi
 if [ ! -e "/koishi/package.json" ]; then
   cp -r /boilerplate/. /koishi/
   sed -Ei 's/(([[:space:]]*)maxPort.*)/\1\n\2host: 0.0.0.0/' /koishi/koishi.yml
+  yarn install
 fi
 
 exec "$@"
