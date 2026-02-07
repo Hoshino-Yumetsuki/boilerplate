46157f8fe4074c1513b8a458d92e7184aabba82e
Hoshino-Yumetsuki <hoshino-yumetsuki@outlook.com>
2026-02-07 18:24:20 +0800

    feat: 更新 yarn，更新 node 版本

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,8 +2,9 @@
 set -eu
 
 chown -R root:root /koishi
+
 if [ ! -e "/koishi/package.json" ]; then
-  unzip -d /koishi /boilerplate.zip
+  cp -r /boilerplate/. /koishi/
   sed -Ei 's/(([[:space:]]*)maxPort.*)/\1\n\2host: 0.0.0.0/' /koishi/koishi.yml
 fi
 
